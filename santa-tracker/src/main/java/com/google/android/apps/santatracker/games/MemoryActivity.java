133a1b4bdd365111235bd45838cbc28cea480716
James Lau <jameslau@google.com>
2017-04-24 17:01:21 -0700

    Applied tips to speed up build times.

--- santa-tracker/src/main/java/com/google/android/apps/santatracker/games/MemoryActivity.java
+++ santa-tracker/src/main/java/com/google/android/apps/santatracker/games/MemoryActivity.java
@@ -17,6 +17,7 @@
 package com.google.android.apps.santatracker.games;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.apps.santatracker.R;
 import com.google.android.apps.santatracker.games.common.PlayGamesActivity;
@@ -50,6 +51,9 @@ public class MemoryActivity extends PlayGamesActivity {
 
         // [ANALYTICS SCREEN]: Memory
         AnalyticsManager.sendScreenView(R.string.analytics_screen_memory);
+        Log.d("Test", "test");
+        Log.d("Test", "test");
+
     }
 
     @Override
